ee983329a0099a2e87d59b111d05beca9418811a
Lim Yue Chuan <shasderias@gmail.com>
2021-09-14 06:22:56 +0800

    implement TakeSet and release v0.0.11

--- transform/lightidset.go
+++ transform/lightidset.go
@@ -24,3 +24,34 @@ func (f flatten) do(set lightid.Set) lightid.Set {
 func (f flatten) LightTransform(l context.Light) context.Light {
 	return applyLightIDSetTransformer(l, f.do, false)
 }
+
+type takeSet struct {
+	idx      []int
+	sequence bool
+}
+
+// TakeSet returns a transformer that takes (returns only) the light ID sets at
+// indexes.
+//
+// [1,2,3], [4,5,6] ... [28,29,30]
+// TakeSet(0, 9)
+// [1,2,3], [28,29,30]
+func TakeSet(indexes ...int) takeSet {
+	return takeSet{indexes, false}
+}
+
+func (t takeSet) do(set lightid.Set) lightid.Set {
+	newSet := lightid.NewSet()
+	for _, idx := range t.idx {
+		newSet.Add(set.Index(idx))
+	}
+	return newSet
+}
+
+func (t takeSet) LightTransform(l context.Light) context.Light {
+	return applyLightIDSetTransformer(l, t.do, t.sequence)
+}
+
+func (t takeSet) Sequence() takeSet {
+	return takeSet{t.idx, true}
+}
