ec6ff76f0c1cc76d8e5b59ce2f8e09141d5fe3b6
Lim Yue Chuan <shasderias@gmail.com>
2022-04-05 07:57:18 +0800

    omitempty precise zoom speed

--- chroma/precisezoom.go
+++ chroma/precisezoom.go
@@ -4,7 +4,7 @@ import "encoding/json"
 
 type PreciseZoom struct {
 	Step  float64 `json:"_step"`
-	Speed float64 `json:"_speed"`
+	Speed float64 `json:"_speed,omitempty"`
 }
 
 func (e *PreciseZoom) CustomData() (json.RawMessage, error) { return json.Marshal(e) }
